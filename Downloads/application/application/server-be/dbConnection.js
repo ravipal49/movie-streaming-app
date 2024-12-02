50dab5d4dc9a8db236789e3a4f64b79eeb79b5f5
ravipal <rvpal5751@gmail.com>
2024-12-02 18:41:06 +0530

    the code

---
+++ Downloads/application/application/server-be/dbConnection.js
@@ -0,0 +1,21 @@
+const mongoose= require ('mongoose')
+
+function connectDatabase() {
+
+const mongoUri =process.env.MONGODB_URI
+
+
+mongoose.connect(mongoUri)
+.then(()=>{
+    console.log('database is connected')
+})
+
+
+
+
+
+}
+module.exports ={
+    connectDatabase
+}
+
